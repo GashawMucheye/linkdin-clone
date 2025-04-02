7c09f6290c36cc02ff33bd9ee83d056dd3348e3a
gashaw mucheye <gashaw2533@gmail.com>
2025-04-02 09:44:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LINKEDIN-clone
+
+This is a clone of linkedin. It is a full stack application with react and nodejs.
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running now on port ${PORT}`.bgYellow.underline.bold);
+});
