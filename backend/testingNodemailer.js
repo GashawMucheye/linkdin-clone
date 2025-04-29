b716649837381ff89373b412d2484ebe920977d9
gashaw mucheye <gashaw2533@gmail.com>
2025-04-29 02:01:39 +0200

    deleted testingNodemailer.js

--- backend/testingNodemailer.js
+++
@@ -1,8 +0,0 @@
-// // Send email
-// transporter.sendMail(mailOptions, (error, info) => {
-//   if (error) {
-//     console.log('Error:', error);
-//   } else {
-//     console.log('Email sent:', info.response);
-//   }
-// });
